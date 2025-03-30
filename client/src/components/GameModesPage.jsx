57514b77f71f3d1d4fd105b5a7c1d9df53515402
SujalBhatt <vimlabhatt97@gmail.com>
2025-03-30 20:57:15 +0530

    creating-components

---
+++ client/src/components/GameModesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+
+const GameModesPage = () => {
+    return (
+        <div>
+            <Link to ="/match-making"><h1>Game Modes Page</h1></Link>
+        </div>
+    );
+};
+
+export default GameModesPage;
---
+++ client/src/components/OneVOnePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+const OneVOnePage = () => {
+    return (
+        <div>
+            <Link to = "/profile"><h1>1v1 Quiz Interface</h1></Link>
+        </div>
+    );
+};
+
+export default OneVOnePage;
